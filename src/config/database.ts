d4aa6e5590041094eaf7ec8bac21aea24e24d8ad
Rahul <rahulratnam1812@gmail.com>
2026-04-27 23:43:28 +0530

    database ts file modified

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,16 +2,20 @@ import { Sequelize } from "sequelize-typescript";
 import User from "../models/user";
 import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER } from "./app.config";
 
+if (!DB_DATABASE || !DB_USER || !DB_HOST) {
+  throw new Error(
+    `❌ Missing DB config — DB_DATABASE: ${DB_DATABASE}, DB_USER: ${DB_USER}, DB_HOST: ${DB_HOST}`
+  );
+}
 
-const sequelize = new Sequelize(
-    DB_DATABASE,
-    DB_USER,
-    DB_PASSWORD,
+export const sequelize = new Sequelize(
     {
-    host:DB_HOST,
-    dialect:'postgres', 
-    port: 5432,
-    models:[User]
-});
+        database: DB_DATABASE,
+        username: DB_USER,
+        password: DB_PASSWORD,
+        host: DB_HOST,
+        dialect: 'postgres',
+        port: 5432,
+        models: [User]
+    });
 
-export default sequelize;
